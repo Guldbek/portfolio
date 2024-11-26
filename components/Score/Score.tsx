20578e5c96161d377f2661da2ccf788c5991132f
Guldbek <guldbek26@gmail.com>
2024-11-26 17:12:10 +0100

    Update Score.tsx

--- components/Score/Score.tsx
+++ components/Score/Score.tsx
@@ -14,13 +14,13 @@ const Score: React.FC = () => {
     return (
         <div className="flex flex-row items-center relative">
                 <div
-                    className="w-1/2 h-screen font-bold bg-red-700 text-white p-4 text-8xl flex items-center justify-center  cursor-pointer"
+                    className="w-1/2 h-screen font-bold bg-red-700 text-white p-4 text-[180px] flex items-center justify-center  cursor-pointer"
                     onClick={() => setRedScore(redScore + 1)}
                 >
                     {redScore}
                 </div>
                 <div
-                    className="w-1/2 h-screen font-bold bg-blue-700 text-white p-4 text-8xl flex items-center justify-center cursor-pointer"
+                    className="w-1/2 h-screen font-bold bg-blue-700 text-white p-4 text-[180px] flex items-center justify-center cursor-pointer"
                     onClick={() => setBlueScore(blueScore + 1)}
                 >
                     {blueScore}
